f1e16370a7981a8c3efa412e463eb3a747a166be
Facundo Ciccioli <facundo.ciccioli@canonical.com>
2021-05-18 16:03:24 +0200

    First "working" version: thruk image is pulled and thruk's started

--- src/charm.py
+++ src/charm.py
@@ -29,12 +29,12 @@ class SidecarCharmThrukCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
+        self.framework.observe(self.on.thruk_pebble_ready, self._on_thruk_pebble_ready)
+        #self.framework.observe(self.on.config_changed, self._on_config_changed)
+        #self.framework.observe(self.on.fortune_action, self._on_fortune_action)
         self._stored.set_default(things=[])
 
-    def _on_httpbin_pebble_ready(self, event):
+    def _on_thruk_pebble_ready(self, event):
         """Define and start a workload using the Pebble API.
 
         TEMPLATE-TODO: change this example to suit your needs.
@@ -48,20 +48,19 @@ class SidecarCharmThrukCharm(CharmBase):
         container = event.workload
         # Define an initial Pebble layer configuration
         pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+            "summary": "thruk layer",
+            "description": "pebble config layer for thruk",
             "services": {
-                "httpbin": {
+                "thruk": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "thruk",
+                    "command": "/usr/src/start.sh",
                     "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
                 }
             },
         }
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
+        container.add_layer("thruk", pebble_layer, combine=True)
         # Autostart any services that were defined with startup: enabled
         container.autostart()
         # Learn more about statuses in the SDK docs:
